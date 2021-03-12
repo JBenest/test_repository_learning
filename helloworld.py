293f3030866921f48de61e583926bda279df050d
JB <gabblegockonly4@7hugsandaturniponalemon.com>
2021-03-12 14:59:08 +0000

    Included new function.

--- helloworld.py
+++ helloworld.py
@@ -3,3 +3,9 @@ print('I hope that your day is good!')
 print('How are you?')
 print('Where do you live?')
 print('What time is it today?')
+
+
+def my_function(a,b):
+    return a+b
+
+print(my_function(2,5))
